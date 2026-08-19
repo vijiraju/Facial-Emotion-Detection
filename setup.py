c455afcff845930123dfef840a4e189fd4b25fd8
vijiraju <vijiraju1947@gmail.com>
2026-08-19 23:50:33 +0530

    Added new feature

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="facial_emotion_detection_system",
     version="0.1.0",
     author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author_email="lakshmiraju96543@gmail.com",
     packages= find_packages(),
     install_requires=[]
 )
